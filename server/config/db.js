fdac38ff488eb58d8ba7006d8dd1b3164566b3b9
PsychoMo <zymike09@hotmail.com>
2019-04-04 14:52:06 -0400

    connected to mlab

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-   "URI" : "mongodb://localhost/test",
-    //"URI" : "mongodb://Admin:Admin123@ds042138.mlab.com:42138/zeyuma-assignment02",
+   //"URI" : "mongodb://localhost/test",
+    "URI" : "mongodb://Musketeer:musketeer123@ds035747.mlab.com:35747/comp308-w2019-musketeer",
     "secret": "SomeSecret"
 }
